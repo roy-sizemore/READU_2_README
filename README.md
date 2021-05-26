4b462f8a5c4010862b5d0ea277b17b54d3e31719
Roy Sizemore <rsizemore@swampcastle.home>
2021-05-26 15:35:11 -0400

    Fixed questions array and inquirer prompt to align with starter code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# READU_2_README
-OSU Bootcamp Week 9 Homework
+# e
+  
--- index.js
+++ index.js
@@ -1,97 +1,85 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+// const path = require('path');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    'Please enter your GitHub username',
-    'Please enter your preferred contact email',
-    'What would you like to call your project?',
-    'Please enter a description of your project',
-    'Please enter the installation instructions for your project',
-    'Please enter the usage information for your project',
-    'Please enter the contribution guidelines for your project',
-    'Please enter the testing instructions for you project?',
-    'Which license are you using for this project?'
-    ];
-
-inquirer
-    .prompt([
-        {
-            name: 'username',
-            message: questions[0],
-            type: 'input'
-        },
-        {
-            name: 'email',
-            message: questions[1],
-            type: 'input'
-        },
-        {
-            name: 'title',
-            message: questions[2],
-            type: 'input'
-        },
-        {
-            name: 'description',
-            message: questions[3],
-            type: 'input'
-        },
-        {
-            name: 'installation',
-            message: questions[4],
-            type: 'input'
-        },
-        {
-            name: 'usage',
-            message: questions[5],
-            type: 'input'
-        },
-        {
-            name: 'contributing',
-            message: questions[6],
-            type: 'input'
-        },
-        {
-            name: 'tests',
-            message: questions[7],
-            type: 'input'
-        },
-        {
-            name: 'license',
-            message: questions[8],
-            type: 'list',
-            choices: ['MIT',
-                'Apache 2.0',
-                'GNU 3.0',
-                'BSD 2-Clause Simplified',
-                'BSD 3-Clause New/Revised',
-                'Boost 1.0',
-                'Creative Commons Zero Universal 1.0',
-                'Eclipse Public 2.0',
-                'GNU Affero 3.0',
-                'GNU General Public 2.0',
-                'GNU Lesser General Public 2.1',
-                'Mozilla Public 2.0',
-                'Unlicense',
-                "I'm not using a license for this project"
-            ]
+    {
+        name: 'username',
+        message: 'Please enter your GitHub username',
+        type: 'input'
+    },
+    {
+        name: 'email',
+        message: 'Please enter your preferred contact email',
+        type: 'input'
+    },
+    {
+        name: 'title',
+        message: 'What would you like to call your project?',
+        type: 'input'
+    },
+    {
+        name: 'description',
+        message: 'Please enter a description of your project',
+        type: 'input'
+    },
+    {
+        name: 'installation',
+        message: 'Please enter the installation instructions for your project',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        message: 'Please enter the usage information for your project',
+        type: 'input'
+    },
+    {
+        name: 'contributing',
+        message: 'Please enter the contribution guidelines for your project',
+        type: 'input'
+    },
+    {
+        name: 'tests',
+        message: 'Please enter the testing instructions for you project?',
+        type: 'input'
+    },
+    {
+        name: 'license',
+        message: 'Which license are you using for this project?',
+        type: 'list',
+        choices: ['MIT',
+            'Apache 2.0',
+            'GNU 3.0',
+            'BSD 2-Clause Simplified',
+            'BSD 3-Clause New/Revised',
+            'Boost 1.0',
+            'Creative Commons Zero Universal 1.0',
+            'Eclipse Public 2.0',
+            'GNU Affero 3.0',
+            'GNU General Public 2.0',
+            'GNU Lesser General Public 2.1',
+            'Mozilla Public 2.0',
+            'Unlicense',
+            "N/A"
+        ]
     }
-]).then(answers => {
-        console.log(answers);
-
-    })
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, answers) {
-    fs.writeToFile(fileName, (answers) => {
-        return;
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err)
+        };
     });
 };
 
 // TODO: Create a function to initialize app
 function init() {
-
+    inquirer.prompt(questions).then(answers => writeToFile('README.md', generateMarkdown(answers)));
 }
 
 // Function call to initialize app
